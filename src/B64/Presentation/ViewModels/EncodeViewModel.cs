53b35043687d84b246323cb70693f9d5447caab5
lastunicorn <coldblackstar@yahoo.com>
2016-10-19 11:15:44 +0300

    added missing files from the previous commit.

---
+++ src/B64/Presentation/ViewModels/EncodeViewModel.cs
@@ -0,0 +1,74 @@
+﻿// B64
+// Copyright (C) 2016 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using DustInTheWind.B64.Business;
+using DustInTheWind.B64.Presentation.Commands;
+
+namespace DustInTheWind.B64.Presentation.ViewModels
+{
+    internal class EncodeViewModel : ViewModelBase
+    {
+        private readonly ApplicationState applicationState;
+
+        private string encodedText;
+        private volatile bool updateFromBusiness;
+
+        public LoadEncodedFileCommand LoadEncodedFileCommand { get; set; }
+        public SaveEncodedFileCommand SaveEncodedFileCommand { get; set; }
+
+        public string EncodedText
+        {
+            get { return encodedText; }
+            set
+            {
+                if (updateFromBusiness)
+                {
+                    encodedText = value;
+                    OnPropertyChanged("EncodedText");
+                }
+                else
+                {
+                    applicationState.EncodedText = value;
+                }
+            }
+        }
+
+        public EncodeViewModel(ApplicationState applicationState)
+        {
+            this.applicationState = applicationState;
+            applicationState.EncodedTextChanged += HandleEncodedTextChanged;
+
+            LoadEncodedFileCommand = new LoadEncodedFileCommand(applicationState);
+            SaveEncodedFileCommand = new SaveEncodedFileCommand(applicationState);
+        }
+
+        private void HandleEncodedTextChanged(object sender, EventArgs eventArgs)
+        {
+            updateFromBusiness = true;
+            try
+            {
+                EncodedText = applicationState.EncodingError != null
+                    ? "error: " + applicationState.EncodingError.Message
+                    : applicationState.EncodedText;
+            }
+            finally
+            {
+                updateFromBusiness = false;
+            }
+        }
+    }
+}
---
+++ src/B64/Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿// B64
+// Copyright (C) 2016 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Reflection;
+using DustInTheWind.B64.Business;
+
+namespace DustInTheWind.B64.Presentation.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        private string title;
+
+        private volatile bool updateFromBusiness;
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public EncodeViewModel EncodeViewModel { get; set; }
+        public DecodeViewModel DecodeViewModel { get; set; }
+
+        public MainViewModel()
+        {
+            Base64Encoder base64Encoder = new Base64Encoder();
+            ApplicationState applicationState = new ApplicationState(base64Encoder);
+
+            EncodeViewModel = new EncodeViewModel(applicationState);
+            DecodeViewModel = new DecodeViewModel(applicationState);
+
+            Title = GetWindowTitle();
+        }
+
+        private static string GetWindowTitle()
+        {
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Version version = assembly.GetName().Version;
+            return string.Format("Base64 Encoder {0}", version.ToString(2));
+        }
+    }
+}
